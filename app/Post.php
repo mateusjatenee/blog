5e23a7e51c643316148684b7466e639dc5142f40
Mateus Guimarães <mateus.jatene@gmail.com>
2015-09-28 01:36:53 -0300

    sluggable stuff

--- app/Post.php
+++ app/Post.php
@@ -2,10 +2,19 @@
 
 namespace App;
 
+use Cviebrock\EloquentSluggable\SluggableInterface;
+use Cviebrock\EloquentSluggable\SluggableTrait;
 use Illuminate\Database\Eloquent\Model;
 
-class Post extends Model
+class Post extends Model implements SluggableInterface
 {
+    use SluggableTrait;
+
+    protected $sluggable = [
+        'build_from' => 'title',
+        'save_to' => 'slug',
+    ];
+
     public function user()
     {
         return $this->hasOne('App\User');

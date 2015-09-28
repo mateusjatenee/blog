ad238c28da44528bf3de49a174fa91edc77b9ede
Mateus Guimarães <mateus.jatene@gmail.com>
2015-09-28 03:22:27 -0300

    bug fixes

---
+++ resources/views/admin/sidebar.blade.php
@@ -0,0 +1,43 @@
+<!-- Left side column. contains the logo and sidebar -->
+<aside class="main-sidebar">
+
+  <!-- sidebar: style can be found in sidebar.less -->
+  <section class="sidebar">
+
+    <!-- Sidebar user panel (optional) -->
+    <div class="user-panel">
+      <div class="pull-left image">
+        <img src="/bower_components/AdminLTE/dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
+      </div>
+      <div class="pull-left info">
+        <p>Bem vindo(a), {{ Auth::user()->name }}</p>
+      </div>
+    </div>
+
+    <!-- search form (Optional) -->
+    <form action="" method="get" class="sidebar-form">
+      <div class="input-group">
+        <input type="text" name="q" class="form-control" placeholder="Procurar usuário">
+        <span class="input-group-btn">
+          <button type="submit" name="search" id="search-btn" class="btn btn-flat"><i class="fa fa-search"></i></button>
+        </span>
+      </div>
+    </form>
+    <!-- /.search form -->
+
+    <!-- Sidebar Menu -->
+    <ul class="sidebar-menu">
+      <li class="header">MENU</li>
+      <!-- Optionally, you can add icons to the links -->
+      <li class="active"><a href="/admin"><i class="fa fa-link"></i> <span>Início</span></a></li>
+      <li class="treeview">
+        <a href="#"><i class="fa fa-link"></i> <span>Posts</span> <i class="fa fa-angle-left pull-right"></i></a>
+        <ul class="treeview-menu">
+          <li><a href="{!! route('admin.posts.index') !!}">Todos os posts</a></li>
+          <li><a href="{!! route('admin.posts.create') !!}">Adicionar marcação</a></li>
+        </ul>
+      </li>
+    </ul><!-- /.sidebar-menu -->
+  </section>
+  <!-- /.sidebar -->
+</aside>

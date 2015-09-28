ee469ec48061def2a4dd29bfb791ad1f9d2b4781
Mateus Guimarães <mateus.jatene@gmail.com>
2015-09-28 01:44:16 -0300

    nah

---
+++ resources/views/blog/index.blade.php
@@ -0,0 +1,32 @@
+@extends('blog.app')
+
+@section('title', 'Início')
+
+@section('contact')
+<!-- Main Content -->
+<div class="container">
+    <div class="row">
+        <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
+        @foreach($posts as $post)
+            <div class="post-preview">
+                <a href="{{ $post->slug }}">
+                    <h2 class="post-title">
+                        {{ $post->title }}
+                    </h2>
+                    <h3 class="post-subtitle">
+                        {{ Markdown::parse($post->content) }}
+                    </h3>
+                </a>
+                <p class="post-meta"><a href="#">{{ route('user.show', [$post->user->id]) }}</a> on September 24, 2014</p>
+            </div>
+            <hr>
+            <!-- Pager -->
+            <ul class="pager">
+                <li class="next">
+                    <a href="#">Older Posts &rarr;</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</div>
+@stop

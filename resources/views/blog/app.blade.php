cff88b1cd3c4a3605071ff9a68fb52f1fe8a504d
Mateus Guimarães <mateus.jatene@gmail.com>
2015-09-28 04:09:28 -0300

    bug fixes

--- resources/views/blog/app.blade.php
+++ resources/views/blog/app.blade.php
@@ -75,7 +75,7 @@
                 <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
                     <div class="site-heading">
                         <h1>
-                        @if (Route::getCurrentRoute()->getPath() == 'blog')
+                        @if (Route::getCurrentRoute()->getPath() == 'blog' || Route::getCurrentRoute()->getPath() == 'blog/about')
                           texto padrão
                         @else
                             {{ $post->title }}
